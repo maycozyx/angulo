8fba5db6e18083cba015eff27e5b5fa20749e1e4
“Mayco” <mayco.zyx@gmail.com>
2018-04-17 09:53:30 -0300

    angulo.php - importando dados por explode

--- angulo.php
+++ angulo.php
@@ -3,5 +3,17 @@
 	<title></title>
 </head>
 <body>
+<?php
+echo "The current date is ";
+echo date("l F d, Y");
+
+
+$arq = file("CADASTRO.csv");
+foreach ($arq as $temp_valor) {
+      list($col1, $col2, $col3) = explode(";", $temp_valor);
+      echo $col1 . " - " . $col2 . " - " . $col3 . "<BR>";
+}
+
+?>
 </body>
 </html>
