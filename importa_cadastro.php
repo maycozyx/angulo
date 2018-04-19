675113a0e6716efea5f03b88b2bce5485fa6d172
“Mayco” <mayco.zyx@gmail.com>
2018-04-19 08:46:37 -0300

    insere cadastro e gabaritos no banco de dados

--- importa_cadastro.php
+++ importa_cadastro.php
@@ -5,6 +5,12 @@
 </head>
 <body>
 <?php
+include "conecta_mysql.inc";
+
+function insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23) {
+	
+	mysql_query("INSERT INTO cadastro ( c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23 ) VALUES ( '$c1', '$c2', '$c3', '$c4', '$c5', '$c6', '$c7', '$c8', '$c9', '$c10', '$c11', '$c12', '$c13', '$c14', '$c15', '$c16', '$c17', '$c18', '$c19', '$c20', '$c21', '$c22', '$c23' )");
+}
 
 $arq = file("../2801_CADASTRO.csv");
 foreach ($arq as $temp_valor) { // c = coluna
@@ -12,8 +18,10 @@ foreach ($arq as $temp_valor) { // c = coluna
       echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
  " - " . $c11 . " - " . $c12 . " - " . $c13 . " - " . $c14 . " - " . $c15 . " - " . $c16 . " - " . $c17 . " - " . $c18 . " - " . $c19 . " - " . $c20 .
  " - " . $c21 . " - " . $c22 . " - " . $c23 . "<BR>";
+//	insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23);
 }
 
+mysql_close($conexao);
 ?>
 </body>
 </html>
--- importa_gabaritos.php
+++ importa_gabaritos.php
@@ -5,17 +5,22 @@
 </head>
 <body>
 <?php
-//echo "The current date is ";
-//echo date("l F d, Y");
+include "conecta_mysql.inc";
 
+function insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13) {
+	
+	mysql_query("INSERT INTO gabaritos ( c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13 ) VALUES ( '$c1', '$c2', '$c3', '$c4', '$c5', '$c6', '$c7', '$c8', '$c9', '$c10', '$c11', '$c12', '$c13' )");
+}
 
 $arq = file("../fr2801.csv");
 foreach ($arq as $temp_valor) { // c = coluna
-      list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14) = explode(",", $temp_valor);
-      echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
- " - " . $c11 . " - " . $c12 . " - " . $c13 . " - " . $c14 . "<BR>";
+    list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13 ) = explode(",", $temp_valor);
+    echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
+ " - " . $c11 . " - " . $c12 . " - " . $c13 . "<BR>";
+	
+//	insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13);
 }
-
+mysql_close($conexao);
 ?>
 </body>
 </html>
