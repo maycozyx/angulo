7d2c980209965b1b1bedd082c4a2606c55eb7a78
“Mayco” <mayco.zyx@gmail.com>
2018-04-23 09:40:13 -0300

    Mostra resultado 6S P1 e P2

--- resultado.php
+++ resultado.php
@@ -6,6 +6,9 @@ ini_set('display_errors', 1 );
 <head>
 	<title></title>
 	<meta charset="utf-8">
+	<style>
+		.break { page-break-before: always; }
+	</style>
 </head>
 <body>
 <pre>
@@ -15,21 +18,70 @@ include "gabarito_provas.inc";
 
 // ------------------ FUNÇÕES
 
-function confere( $a, $qi, $qf, $d ) { // a = Aluno; qi = Questão Inicial, qf = Questão Final; d = Disciplina 
-	// fazer uma função que retorna o gabarito específico
+function cp( $tq, $ta ) { // Calcula quantos Porcento o aluno teve de acerto; tq = Total de Questões; ta = Total de Acertos
+	return round( ( ($ta * 100) / $tq ), 2 );
 }
 
-function processa( $sp, $t, $qi, $qf, $d ) { // Processa notas da turma; sp = Série Prova; t = turma; qi = Questão Inicial, qf = Questão Final; d = Disciplina 
+function confere( $sp, $t, $a, $qi, $qf, $d, $spg ) { // a = Aluno; qi = Questão Inicial, qf = Questão Final; d = Disciplina 
+	//print_r($spg['D6EF01']['B']);
+	//print_r($a['respostas']);
+	
+	$acertos = 0; // armazena a quantidade de acertos do aluno na prova
+	$i = $qi; // para controle do while
+	$qq = 0; // qq = Quantidade de Questões, p/ calcular a % de acertos
+	$rga = str_split($a['respostas']); // rga = Respostas do Gabarito do Aluno
+	//print_r($rga);
+	//print_r($spg[$sp][$t]);
+	echo "$sp $t $d ";
+	while ( ( $i >= $qi ) && ( $i <= $qf ) ) {
+		if ( ($rga[$i-1] == $spg[$sp][$t][$i-1]) || ($spg[$sp][$t][$i-1] == 'Z') ) {
+			//echo "*";
+			$acertos++;
+		}
+		
+		echo "<b>".$spg[$sp][$t][$i-1].'</b>'.$rga[$i-1].' '; // -1 porque o array começa com zero
+		//echo $i;
+		$i++;
+		$qq++;
+	}
+	echo "<b>Q:</b>$qq <b>A:</b>$acertos ".cp($qq, $acertos). '% ' .$a['nome'].'<br>';
+}
+
+function processa( $sp, $t, $qi, $qf, $d, $spg) { // Processa notas da turma; sp = Série Prova; t = turma; qi = Questão Inicial, qf = Questão Final; d = Disciplina 
 		$rq = mysql_query("SELECT respostas, serie_prova, numero, turma, nome FROM selecionados WHERE serie_prova='$sp' AND turma='$t' ORDER BY turma ASC, nome ASC"); // rq = Resultado Query 
 		while ( $row = mysql_fetch_array( $rq ) )
         {
-			confere( $row, $qi, $qf, $d );
+			confere( $sp, $t, $row, $qi, $qf, $d, $spg );		
 		}
+		echo "<br class='break'>";
 }
 
 // ------------------ PRINCIPAL 
 
-processa( 'D6EF01', 'A', 1, 7, 'Matemática' );
+//print_r($spg['D6EF01']);
+
+foreach( $spg['D6EF01'] as $t => $value ) { // faz para todas as turmas do 6 ano prova 1
+	//print_r($t);
+	processa( 'D6EF01', $t, 1, 7, 'Matemática', $spg);
+	processa( 'D6EF01', $t, 8, 11, 'História', $spg);
+	processa( 'D6EF01', $t, 12, 15, 'Geografia', $spg);
+}
+
+foreach( $spg['D6EF02'] as $t => $value ) { // faz para todas as turmas do 6 ano prova 1
+	processa( 'D6EF02', $t, 1, 10, 'Português', $spg);
+	processa( 'D6EF02', $t, 11, 15, 'Ciências', $spg);
+}
+
+
+//echo '<br>';
+
+//processa( 'D6EF01', 'A', 1, 7, 'Matemática', $spg);
+//processa( 'D6EF01', 'A', 8, 11, 'História', $spg);
+//processa( 'D6EF01', 'A', 12, 15, 'Geografia', $spg);
+
+//$a = 'D6EF01';
+//print_r($spg[$a]);
+//print_r($spg);
 
 
 mysql_close($conexao);
