3b4ab65f6c092d42809a5116f618d65f1e471b52
Mayco <mayco.zyx@gmail.com>
2018-06-28 17:24:55 -0300

    P3 e P4

--- resultados/importa_cadastro.php
+++ resultados/importa_cadastro.php
@@ -15,9 +15,9 @@ function insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c
 	mysql_query("INSERT INTO cadastro ( c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23 ) VALUES ( '$c1', '$c2', '$c3', '$c4', '$c5', '$c6', '$c7', '$c8', '$c9', '$c10', '$c11', '$c12', '$c13', '$c14', '$c15', '$c16', '$c17', '$c18', '$c19', '$c20', '$c21', '$c22', '$c23' )");
 }
 
-$arq = file("../2801_CADASTRO.csv");
+$arq = file("../../2801_CADASTRO.csv");
 foreach ($arq as $temp_valor) { // c = coluna
-      list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23) = explode(",", $temp_valor);
+      list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23) = explode(";", $temp_valor);
       echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
  " - " . $c11 . " - " . $c12 . " - " . $c13 . " - " . $c14 . " - " . $c15 . " - " . $c16 . " - " . $c17 . " - " . $c18 . " - " . $c19 . " - " . $c20 .
  " - " . $c21 . " - " . $c22 . " - " . $c23 . "<BR>";
--- resultados/importa_gabaritos.php
+++ resultados/importa_gabaritos.php
@@ -15,7 +15,7 @@ function insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c
 	mysql_query("INSERT INTO gabaritos ( c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13 ) VALUES ( '$c1', '$c2', '$c3', '$c4', '$c5', '$c6', '$c7', '$c8', '$c9', '$c10', '$c11', '$c12', '$c13' )");
 }
 
-$arq = file("../fr2801.csv");
+$arq = file("../../fr2801.csv");
 foreach ($arq as $temp_valor) { // c = coluna
     list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13 ) = explode(",", $temp_valor);
     echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
