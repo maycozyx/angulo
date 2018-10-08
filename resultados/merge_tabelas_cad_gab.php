74f36a520f2e61453f42c8e7644da11f19162ce0
Mayco <mayco.zyx@gmail.com>
2018-10-08 17:21:49 -0300

    Atualização mysql para mysqli

--- resultados/merge_tabelas_cad_gab.php
+++ resultados/merge_tabelas_cad_gab.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL ^ E_DEPRECATED);
-ini_set('display_errors', 1 );
+//error_reporting(E_ALL ^ E_DEPRECATED);
+//ini_set('display_errors', 1 );
 ?><!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -37,22 +37,22 @@ function lsp( $sp ) { // sp = Série Prova; lsp = Limpa dados Série Prova para 
 	return $spl;
 }
 */
-$rg = mysql_query("SELECT c2, c3, c10, c13 FROM gabaritos ORDER BY c2 ASC, c10 ASC, c3 ASC"); // rg = resultado gabaritos
-while ($rowg=mysql_fetch_array($rg))
+$rg = mysqli_query($conexao, "SELECT c2, c3, c10, c13 FROM gabaritos ORDER BY c2 ASC, c10 ASC, c3 ASC"); // rg = resultado gabaritos
+while ($rowg=mysqli_fetch_array($rg))
         {
 			// busca nome
-			$rc = mysql_query("SELECT c2 FROM cadastro WHERE c4='$rowg[c3]'"); // rc = resultado cadastro
-			$rowc=mysql_fetch_array($rc); // rowc = linha do cadastro contendo o nome 
+			$rc = mysqli_query($conexao, "SELECT c2 FROM cadastro WHERE c4='$rowg[c3]'"); // rc = resultado cadastro
+			$rowc=mysqli_fetch_array($rc); // rowc = linha do cadastro contendo o nome 
 			$rga = gabaritoAlunno( $rowg['c2'], $rowg['c13'], $spg ); // rga = Respostas do Gabarito do Aluno
 			echo "<b>R:</b>$rga <b>SP:</b>$rowg[c2] <b>Nº</b>$rowg[c3] <b>T:</b>$rowg[c10] <b>N:</b>$rowc[0]";
 			/* $spl = lsp(); // spl = Série Prova Limpa  */
-			mysql_query( "INSERT INTO selecionados ( respostas, serie_prova, numero, turma, nome ) VALUES ( '$rga', '$rowg[c2]', '$rowg[c3]', '$rowg[c10]', '$rowc[0]' )" );
-			printf(" Records inseridos: %d\n", mysql_affected_rows()); 
-			echo mysql_error();
+			mysqli_query($conexao, "INSERT INTO selecionados ( respostas, serie_prova, numero, turma, nome ) VALUES ( '$rga', '$rowg[c2]', '$rowg[c3]', '$rowg[c10]', '$rowc[0]' )" );
+			printf(" Records inseridos: %d\n", mysqli_affected_rows($conexao)); 
+			echo mysqli_error($conexao);
 			echo "<br>";
         }        
 
-mysql_close($conexao);
+mysqli_close($conexao);
 ?>
 </pre>
 </body>
