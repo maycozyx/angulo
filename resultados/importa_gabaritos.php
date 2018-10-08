38e5ba0cbf495f56d1eb1b7c658e15cd1cb22e9e
Mayco <mayco.zyx@gmail.com>
2018-10-08 16:39:36 -0300

    Atualização mysql para mysqli

--- resultados/importa_gabaritos.php
+++ resultados/importa_gabaritos.php
@@ -10,20 +10,34 @@ ini_set('display_errors', 1 );
 <?php
 include "conecta_mysql.inc";
 
-function insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13) {
+function insere_dado($conexao, $c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13) {
 	
-	mysql_query("INSERT INTO gabaritos ( c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13 ) VALUES ( '$c1', '$c2', '$c3', '$c4', '$c5', '$c6', '$c7', '$c8', '$c9', '$c10', '$c11', '$c12', '$c13' )");
+	mysqli_query($conexao, "INSERT INTO gabaritos ( c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13 ) VALUES ( '$c1', '$c2', '$c3', '$c4', '$c5', '$c6', '$c7', '$c8', '$c9', '$c10', '$c11', '$c12', '$c13' )");
 }
 
-$arq = file("../../fr2801.csv");
-foreach ($arq as $temp_valor) { // c = coluna
-    list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13 ) = explode(",", $temp_valor);
-    echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
+function abre_arquivo($conexao, $nome_arquivo) {
+	
+	$arq = file($nome_arquivo);
+	foreach ($arq as $temp_valor) { // c = coluna
+		list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13 ) = explode(",", $temp_valor);
+		echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
  " - " . $c11 . " - " . $c12 . " - " . $c13 . "<BR>";
 	
-	insere_dado($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13);
+		insere_dado($conexao, $c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13);
+	}
 }
-mysql_close($conexao);
+//-------- PROGRAMA PRINCIPAL ------
+
+abre_arquivo($conexao, "../../2801-EF6P5.csv");
+abre_arquivo($conexao, "../../2801-EF6P6.csv");
+abre_arquivo($conexao, "../../2801-EF7P5.csv");
+abre_arquivo($conexao, "../../2801-EF7P6.csv");
+abre_arquivo($conexao, "../../2801-EF8P5.csv");
+abre_arquivo($conexao, "../../2801-EF8P6.csv");
+abre_arquivo($conexao, "../../2801-EF9P5.csv");
+abre_arquivo($conexao, "../../2801-EF9P6.csv");
+
+mysqli_close($conexao);
 ?>
 </body>
 </html>
