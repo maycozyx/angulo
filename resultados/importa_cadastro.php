39b8345b3c1aff8849539806dad816797733b098
Mayco <mayco.zyx@gmail.com>
2018-10-09 09:10:52 -0300

    Inclui no banco de dados apenas os cadastros ativos = *

--- resultados/importa_cadastro.php
+++ resultados/importa_cadastro.php
@@ -20,8 +20,12 @@ foreach ($arq as $temp_valor) { // c = coluna
       list($c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23) = explode(";", $temp_valor);
       echo $c1 . " - " . $c2 . " - " . $c3 . " - " . $c4 . " - " . $c5 . " - " . $c6 . " - " . $c7 . " - " . $c8 . " - " . $c9 . " - " . $c10 .
  " - " . $c11 . " - " . $c12 . " - " . $c13 . " - " . $c14 . " - " . $c15 . " - " . $c16 . " - " . $c17 . " - " . $c18 . " - " . $c19 . " - " . $c20 .
- " - " . $c21 . " - " . $c22 . " - " . $c23 . "<BR>";
-	insere_dado($conexao, $c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23);
+ " - " . $c21 . " - " . $c22 . " - " . $c23;
+	if ( $c1 == "*") { // se for um cadastro ativo, então insere no banco de dados
+		insere_dado($conexao, $c1, $c2, $c3, $c4, $c5, $c6, $c7, $c8, $c9, $c10, $c11, $c12, $c13, $c14, $c15, $c16, $c17, $c18, $c19, $c20, $c21, $c22, $c23);
+		echo " INCLUÍDO";
+	}
+	echo "<BR>";
 }
 
 mysqli_close($conexao);
